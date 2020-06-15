2b124412b1ac83179d34c9e9f8f27b62c2acde04
ligatorDaniel <danielulloa256@gmail.com>
2020-06-15 17:25:25 -0400

    agregamos editar usuario

--- controllers/user.js
+++ controllers/user.js
@@ -84,8 +84,29 @@ const loginUser = (req, res) => {
     });
 }
 /*Login usuario*/
+/*Editar usuario*/
+function updateUser(req,res){
+    var userId = req.params.id;
+    var update = req.body;
+
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+        if(err){
+            res.status(500).send({message: 'Error del servidor, al actualizar usuario'});
+        }else{
+            if(!userUpdated){
+                res.status(404).send({message: 'no se ha podido actualizar el usuario'});
+            }else{
+                res.status(200).send({user: userUpdated});
+            }
+        }
+    });
+}
+/*Editar usuario*/
+
+
 module.exports = {
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    updateUser,
 }
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,12 @@ const express = require('express');
 const userController = require('../controllers/user');
 
 const api = express.Router();
-const md_auth = require('../middlewares/authenticated');
+const md_auth = require('../middlewares/authenticated'); /*nos permite usar md_auth.ensureAuth*/
 
 api.get('/probando-controlador',md_auth.ensureAuth, userController.pruebas);
 api.post('/register', userController.saveUser);
 api.post('/login', userController.loginUser);
+api.put('/update-user/:id', md_auth.ensureAuth, userController.updateUser);
 
 module.exports = api;
 
