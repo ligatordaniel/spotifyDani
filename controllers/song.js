0b6fea659930bd510a495a720e8ca7b343542575
ligatorDaniel <danielulloa256@gmail.com>
2020-06-19 13:01:42 -0400

    get 1 song agregado listo

--- controllers/song.js
+++ controllers/song.js
@@ -8,17 +8,30 @@ const Album = require('../models/album');
 const Song = require('../models/song');
 
 
-/*Get 1 Song*/
+/*Get 1 Song*/ 
 const getSong = (req, res) => {
-    res.status(200).send({message: 'este controlador funciona'});
+    const songId = req.params.id; /*id de la url*/
+    /*metodo populate: popule los datos asosciados al id artist en la db*/
+    /*metodo exec: ejecuta una busqueda sobre las coincidencias de una expresión regular en una cadena especifica. Devuelve el resultado como array, o null.*/
+    Song.findById(songId).populate({path: 'album'}).exec((err, song) => {
+        if(err){
+            res.status(500).send({message: 'Error en la peticion -servidor-'});
+        }else{
+            if(!song){
+                res.status(404).send({message: 'la cancion no existe'});
+            }else{
+                res.status(200).send({song});
+            }
+        }
+    });
 }  
 /*Get 1 Song*/
 
 /*Guardar song*/
 const saveSong = (req, res) => {
-    let song = new Song();
+    const song = new Song();
 
-    let params = req.body;
+    const params = req.body;
     song.number = params.number;
     song.name = params.name;
     song.duration = params.duration;
--- routes/song.js
+++ routes/song.js
@@ -9,7 +9,7 @@ const md_auth = require ('../middlewares/authenticated');
 const multipart = require('connect-multiparty'); /*necesario para img subidas song*/
 const md_upload = multipart({ uploadDir: './uploads/song'});
 
-api.get('/song', md_auth.ensureAuth, SongController.getSong);
+api.get('/song/:id', md_auth.ensureAuth, SongController.getSong);
 api.post('/song', md_auth.ensureAuth, SongController.saveSong);
 
 
