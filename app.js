820c4100d61f7987752ca47ce75d492ca346d75b
ligatorDaniel <danielulloa256@gmail.com>
2020-06-22 17:20:11 -0400

    cabeceras CORS agregadas

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ const song_routes = require('./routes/song');
 app.use(bodyParser.urlencoded({extended:false}));   /*configuracion body-parser para trabajar json con http*/
 app.use(bodyParser.json());                         /*configuracion body-parser para trabajar json con http*/
 
-/*Confi cabeceras http*/
+/*Confi cabeceras http*/         /*evita problemas con control del acceso entre otros, etc*/
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY , Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 /*Rutas base*/
 app.use('/api', user_routes);   /*http://localhost:3977/api/probando-controlador*/
